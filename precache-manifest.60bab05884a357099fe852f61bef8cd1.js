4fd45e8648cc5073e49cc9db2a1d4b8f246d36b0
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-16 10:53:17 +0000

    Updates

---
+++ precache-manifest.60bab05884a357099fe852f61bef8cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51757143aae3df1c42053887fb0bf03d",
+    "url": "/deck_of_cards/index.html"
+  },
+  {
+    "revision": "1833a251a4d8cb591e5f",
+    "url": "/deck_of_cards/static/css/main.3c1ebefa.chunk.css"
+  },
+  {
+    "revision": "b3d0eda424c5e9cd77f5",
+    "url": "/deck_of_cards/static/js/2.ee40ccfe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deck_of_cards/static/js/2.ee40ccfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1833a251a4d8cb591e5f",
+    "url": "/deck_of_cards/static/js/main.b6ad1e01.chunk.js"
+  },
+  {
+    "revision": "08e1452241dda51ed9bf",
+    "url": "/deck_of_cards/static/js/runtime-main.fa3a3fb6.js"
+  }
+]);
